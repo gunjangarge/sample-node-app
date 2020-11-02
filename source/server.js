1f4a07f0e4fbe02b071b486894382f6b3bf0a9f2
Gunjan Garge <gunjangarge@gmail.com>
2020-11-02 13:58:23 +0530

    Update server.js

--- source/server.js
+++ source/server.js
@@ -24,7 +24,7 @@ app.get('/perf', (req, res) => {
     console.log("counted till " + i);
     res.status(200)
     .send({
-        message: "counted till " + i"
+        message: "counted till " + i
     });
 })
 
